0c974d5f2a1300ef2410d30dcafab0993c710410
zdn2442 <zdn.2442.ok@gmail.com>
2022-12-16 09:02:49 +0700

    PAS_React

--- src/api/base_url.js
+++ src/api/base_url.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const client = axios.create({
-    baseURL: "http://34.128.70.114",
+    baseURL: "https://sainuu.xyz",
   
     headers: {
       Accept: "application/json",
